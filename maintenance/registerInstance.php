1d41e758d1e47b892f6277967e15bc8645903a14
d.savuljesku <d.savuljesku@gmail.com>
2022-06-08 10:38:48 +0200

    Add "registerInstance" script
    Change-Id: I4067a3a2e768d48e7a8b8cb46dbc12b8de7f45c3
    (cherry picked from commit e6133452a9ca1a442d325d9b700c2c22c54e2ed6)

---
+++ maintenance/registerInstance.php
@@ -0,0 +1,43 @@
+<?php
+
+use MediaWiki\MediaWikiServices;
+
+require_once dirname( dirname( dirname( __DIR__ ) ) ) . '/maintenance/Maintenance.php';
+
+class RegisterInstance extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( 'projectname', '', true, true );
+		$this->addOption( 'groupid', '', false, true );
+	}
+
+	public function execute() {
+		$name = $this->getOption( 'projectname', '' );
+		if ( !$name ) {
+			$this->fatalError( 'Param --projectname must have a value' );
+		}
+		$groupId = (int) $this->getOption( 'groupid', 0 );
+		if ( !$groupId ) {
+			$this->fatalError( 'Param --groupid must have an integer value' );
+		}
+		/** @var \TuleapWikiFarm\InstanceManager $manager */
+		$manager = MediaWikiServices::getInstance()->getService( 'InstanceManager' );
+		if ( !$manager->isCreatable( $name ) ) {
+			$this->fatalError( 'Instance with this name already exists' );
+		}
+		$entity = $manager->getStore()->getInstanceById( $groupId );
+		if ( $entity !== null ) {
+			$this->fatalError( 'Instance for this groupid already exists' );
+		}
+
+		$entity = $manager->getNewInstance( $name, $groupId );
+		if ( !$manager->getStore()->storeEntity( $entity ) ) {
+			$this->fatalError( "Could not register instance" );
+		}
+		$this->output( 'Instance registered' );
+	}
+}
+
+$maintClass = 'RegisterInstance';
+require_once RUN_MAINTENANCE_IF_MAIN;
