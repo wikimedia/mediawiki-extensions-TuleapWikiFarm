4ce72442bda27d59fc08bf1791f876e9bca3e598
d.savuljesku <d.savuljesku@gmail.com>
2022-06-30 14:59:43 +0200

    Allow terminating sessions for only one user
    Change-Id: I656d99e3ccd9c746384038eac2c115a415f21dfd

--- maintenance/terminateAllSessions.php
+++ maintenance/terminateAllSessions.php
@@ -1,15 +1,27 @@
 <?php
 
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Session\SessionManager;
+
 require_once dirname( dirname( dirname( __DIR__ ) ) ) . '/maintenance/Maintenance.php';
 
 class TerminateAllSessions extends Maintenance {
 	private $batchSize = 1000;
 
-	public function execute() {
-		$lbFactory = \MediaWiki\MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
-		$sessionManager = \MediaWiki\Session\SessionManager::singleton();
-		$userFactory = \MediaWiki\MediaWikiServices::getInstance()->getUserFactory();
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( 'user', 'User to invalidate', false, true, 'u' );
+	}
 
+	public function execute() {
+		$user = $this->getOption( 'user' );
+		$userFactory = MediaWikiServices::getInstance()->getUserFactory();
+		if ( $user ) {
+			$this->invalidateForUser( $userFactory->newFromName( $user ) );
+			return;
+		}
+
+		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
 		$users = $lbFactory->getMainLB()->getConnection( DB_REPLICA )->select(
 			'user',
 			[ 'user_name' ],
@@ -20,22 +32,32 @@ class TerminateAllSessions extends Maintenance {
 		$i = 0;
 		foreach ( $users as $userRow ) {
 			$i++;
-			$user = $userFactory->newFromName( $userRow->user_name );
-			try {
-				$sessionManager->invalidateSessionsForUser( $user );
-				if ( $user->getId() ) {
-					$this->output( 'Invalidated session for user ' . $user->getName() . "\n" );
-				} else {
-					$this->output( "Cannot find user {$user->getName()}, tried to invalidate anyways\n" );
-				}
-			} catch ( Exception $ex ) {
-				$this->output( "Failed to invalidate sessions for user {$user->getName()} | "
-					. str_replace( [ "\r", "\n" ], ' ', $ex->getMessage() ) . "\n" );
-			}
+			$this->invalidateForUser( $userFactory->newFromName( $userRow->user_name ) );
 
 			if ( $i % $this->batchSize ) {
 				$lbFactory->waitForReplication();
 			}
+		}
+	}
+
+	/**
+	 * @param User|bool $user
+	 */
+	private function invalidateForUser( $user ) {
+		if ( !( $user instanceof User ) ) {
+			$this->fatalError( "User $user is invalid\n" );
+		}
+		$sessionManager = SessionManager::singleton();
+		try {
+			$sessionManager->invalidateSessionsForUser( $user );
+			if ( $user->getId() ) {
+				$this->output( 'Invalidated session for user ' . $user->getName() . "\n" );
+			} else {
+				$this->output( "Cannot find user {$user->getName()}, tried to invalidate anyways\n" );
+			}
+		} catch ( Exception $ex ) {
+			$this->output( "Failed to invalidate sessions for user {$user->getName()} | "
+				. str_replace( [ "\r", "\n" ], ' ', $ex->getMessage() ) . "\n" );
 		}
 	}
 
