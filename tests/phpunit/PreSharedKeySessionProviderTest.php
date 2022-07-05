65c1ef7039f60d0bc0bb7af6b08a20fea3243e30
rvogel <vogel@hallowelt.biz>
2022-07-05 17:32:36 +0200

    Remove unit-test-exclusive public method
    Change-Id: I2510b73aa5c003d51e465b86e638968698ee7cbe

--- tests/phpunit/PreSharedKeySessionProviderTest.php
+++ tests/phpunit/PreSharedKeySessionProviderTest.php
@@ -2,6 +2,7 @@
 
 namespace TuleapWikiFarm\Tests;
 
+use HashConfig;
 use MediaWiki\Session\SessionInfo;
 use MediaWiki\Session\SessionManager;
 use PHPUnit\Framework\TestCase;
@@ -10,17 +11,21 @@ use TuleapWikiFarm\PreSharedKeySessionProvider;
 class PreSharedKeySessionProviderTest extends TestCase {
 	/**
 	 * @param string $localSecret
-	 * @param string $remoteHeader
+	 * @param string $remoteSecret
+	 * @param int $timeOffset
 	 * @param bool $shouldProvide
 	 * @covers \TuleapWikiFarm\PreSharedKeySessionProvider::provideSessionInfo
 	 * @dataProvider provideSecret
 	 */
-	public function testGetSessionInfo( $localSecret, $remoteHeader, $shouldProvide ) {
-		$provider = new PreSharedKeySessionProvider();
-		$provider->setPreSharedKey( $localSecret );
+	public function testGetSessionInfo( $localSecret, $remoteSecret, $timeOffset, $shouldProvide ) {
+		$config = new HashConfig( [
+			'TuleapPreSharedKey' => $localSecret
+		] );
+		$provider = new PreSharedKeySessionProvider( $config );
 		$provider->setManagerOverride( SessionManager::singleton() );
 
 		$requestMock = $this->createMock( \WebRequest::class );
+		$remoteHeader = 'Bearer ' . hash_hmac( 'sha256', $remoteSecret, time() + $timeOffset );
 		$requestMock->method( 'getHeader' )->willReturnCallback(
 			static function ( $name ) use ( $remoteHeader ) {
 				if ( $name === 'Authorization' ) {
@@ -29,6 +34,13 @@ class PreSharedKeySessionProviderTest extends TestCase {
 				return '';
 			}
 		);
+
+		// Should not run if not in REST context
+		if ( !defined( 'MW_REST_API' ) ) {
+			$info = $provider->provideSessionInfo( $requestMock );
+			$this->assertNull( $info );
+			define( 'MW_REST_API', 1 );
+		}
 
 		$info = $provider->provideSessionInfo( $requestMock );
 
@@ -46,12 +58,20 @@ class PreSharedKeySessionProviderTest extends TestCase {
 		return [
 			'proper-secret' => [
 				'e9971341f03fdc2021f72a89d5a12187',
-				'Bearer ' . hash_hmac( 'sha256', 'e9971341f03fdc2021f72a89d5a12187', time() ),
+				'e9971341f03fdc2021f72a89d5a12187',
+				0,
 				true
 			],
 			'mismatched-secret' => [
 				'e9971341f03fdc2021f72a89d5a12187',
-				'Bearer ' . hash_hmac( 'sha256', 'INVALID', time() ),
+				'INVALID',
+				0,
+				false
+			],
+			'time-leeway-issue' => [
+				'e9971341f03fdc2021f72a89d5a12187',
+				'e9971341f03fdc2021f72a89d5a12187',
+				15,
 				false
 			],
 		];
